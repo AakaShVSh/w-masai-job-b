97807b8fd880e8c2ef0d1feb2f336d16ba2cfa2f
AakaShVSh <97448935+AakaShVSh@users.noreply.github.com>
2022-12-05 22:25:32 +0530

    s

---
+++ server.js
@@ -0,0 +1,11 @@
+const PORT = process.env.PORT || 5000;
+const path = require("path");
+const jsonserver = require("json-server");
+const server = jsonserver.create();
+const router = jsonserver.router(path.join(__dirname,"db.json")); 
+const middlewares = jsonserver.defaults();
+server.use(middlewares);
+server.use(jsonserver.bodyParser);
+server.use("/api",router);
+server.listen(PORT, () => console.log("Running on port 3000"))
+
